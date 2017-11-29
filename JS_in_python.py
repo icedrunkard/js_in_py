55cf36dad51a335be574c5dd5afc905253e8b056
icedrunkard <icedrunkard@163.com>
2017-11-29 23:46:11 +0800

    add call of js_class

--- JS_in_python.py
+++ JS_in_python.py
@@ -4,12 +4,21 @@
 
 #PyExecJs执行单个函数，python 2.7/python 3.6
 import execjs
-from js_content import js
+from js_content import js,js2
 sss='kfj\r\nlhss '
-p=execjs.compile(js)
-s=p.call('encode',sss)
-print('PyExecJs执行结果: ',s)
+#执行一个函数的写法
+p1=execjs.compile(js)
+s1=p1.call('encode',sss)
+print('PyExecJs执行结果(encode): ',s1)
 
+#执行一个类的写法
+p2=execjs.compile(js2)
+s2=p2.call('new Base64().encode',sss)
+print('PyExecJs执行结果(new Base64()): ',s2)
+
+p2=execjs.compile(js2+'var base=new Base64();')
+s2=p2.call('base.encode',sss)
+print('PyExecJs执行结果(base.encode): ',s2)
 
 
 
@@ -17,6 +26,6 @@ print('PyExecJs执行结果: ',s)
 # import PyV8
 # ctxt=PyV8.JSContext()
 # ctxt.enter()
-# ctxt.eval(js)
-# encode=ctxt.locals.encode
+# ctxt.eval(js2+'var base=new Base64();')
+# encode=ctxt.locals.base.encode
 # print('PyV8执行结果: ',encode(sss))
